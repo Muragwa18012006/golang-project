c832a5cad92f342ceff39b55eca08ea63fba2234
Muragwa18012006 <muragwachrissruta@gmail.com>
2024-09-06 20:48:48 +0200

    new-2

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	router := gin.New()
 	/* router.Use(cors.Default()) */
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"}, 
+		AllowOrigins:     []string{"http://localhost:3000","https://next-js-blog-phi-ruddy.vercel.app"}, 
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "token"},
         ExposeHeaders:    []string{"Content-Length"},
